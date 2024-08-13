5c973406176cf34ecc40a24f2c930c76da16d1a8
UINick <nicholas_developer@outlook.com>
2024-08-12 23:48:35 -0300

    feat: init the xcode project and removing main storyboard file

---
+++ Verse Of The Day/Verse Of The DayUITests/Verse_Of_The_DayUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Verse_Of_The_DayUITestsLaunchTests.swift
+//  Verse Of The DayUITests
+//
+//  Created by Nicholas Forte on 12/08/24.
+//
+
+import XCTest
+
+final class Verse_Of_The_DayUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
